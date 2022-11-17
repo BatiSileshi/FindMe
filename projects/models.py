547caa66cf1aa276e55b5a559968aa808b6242b3
jiosaa <batisileshi93@gmail.com>
2022-11-16 23:15:17 -0500

    Find me project started

---
+++ projects/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+import uuid
+# Create your models here.
+class Project(models.Model):
+    title=models.CharField(max_length=200, null=True)
+    description=models.TextField(null=True)
+    demo_link=models.CharField(max_length=3000, null=True, blank=True)
+    source_link=models.CharField(max_length=3000, null=True, blank=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    
+    def __str__(self):
+        return self.title
