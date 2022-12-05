db27c83396886e9f6139871162468f777cdb35ae
jiosaa <batisileshi93@gmail.com>
2022-12-05 13:17:57 -0500

    signals added

--- users/apps.py
+++ users/apps.py
@@ -4,3 +4,14 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+    
+    
+    
+    ## connecting to signals
+
+    def ready(self):
+        import users.signals
+
+
+
+    
